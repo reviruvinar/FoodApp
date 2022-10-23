823cdd544efafe0de0c2f850bf98fd3721d92e86
Revi Ruvina Ratu <reviruvinar@gmail.com>
2022-10-23 22:12:39 +0700

    Build food app

---
+++ README.md
@@ -0,0 +1,10 @@
+# donut_006
+
+Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:food021_app/constants.dart';
+import 'screens/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food App 021',
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
